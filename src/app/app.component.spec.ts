b36f7285760d4831e68774b29d690c352f5bab92
RoryEiffe <roryeiffe8@gmail.com>
2023-05-23 17:30:17 -0400

    Wrote some unit tests and updated the README

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('input-output');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('input-output app is running!');
-  });
 });
